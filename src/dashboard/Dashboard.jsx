9908f2e3c629c4ba35bd1b2b90b4518edfae35d3
Pratham Hebbar <prathamhebbar2021@gmail.com>
2025-07-14 09:11:14 -0500

    begin working on dashboard page

--- src/dashboard/Dashboard.jsx
+++ src/dashboard/Dashboard.jsx
@@ -1,12 +1,65 @@
-import { Typography } from '@mui/material';
-import React, { useState } from 'react';
+import React, { useState, useMemo } from "react";
+import { AppBar, Button, Toolbar, Typography, Box, createTheme, CssBaseline, Stack, ThemeProvider} from '@mui/material';
+import logo_dark from '../assets/smoothie-dark.png';
+import logo_light from '../assets/smoothie-light.png';
+import toggle_dark from '../assets/day.png';
+import toggle_light from '../assets/night.png';
 
 const Dashboard = () => {
 
+    const [themeMode, setThemeMode] = useState('light');
+    
+    const toggleTheme = () => {
+        setThemeMode((prev) => (prev === 'light' ? 'dark' : 'light'));
+    };
+    
+    const theme = useMemo(() => createTheme({
+        palette: {
+            mode: themeMode,
+        },
+    }), [themeMode]);
 
+    const handleAccountDeletion = () => {
+
+    };
+
+    const handleSignOut = () => {
+
+    };
+    
     return (
     <>
-        <Typography>Hello</Typography>
+        <ThemeProvider theme={theme}>
+            <CssBaseline/>
+            <AppBar elevation={0} sx={{ bgcolor: themeMode === 'light' ? '#f4efefff' : '#000000', px: '7%', py: 0.5,}}>
+                <Toolbar sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+                    <Box>
+                        <Stack direction="column" spacing={0.3} alignItems='center'>
+                            <img src={themeMode === 'light' ? logo_light : logo_dark} alt="logo" style={{ width: '36px', filter: themeMode === 'light' ? 'none' : 'brightness(0) invert(1)', cursor: 'pointer',}}/>
+                    
+                            <Typography variant="h6" component="div" sx={{ fontSize: '1rem', color: themeMode === 'light' ? 'black': 'white', mt: '0px', fontWeight: 550, textAlign: 'center' }}>
+                                Nutrify
+                            </Typography>
+                        </Stack>
+                    </Box>
+
+                    <Box sx={{ display: 'flex', alignItems: 'center', gap: 2 }}>
+                        <Stack direction="row" spacing={2}>
+                            <Button onClick={handleSignOut} sx={{ color: themeMode === 'light' ? 'black':'white', backgroundColor: 'rgba(78, 196, 4, 1)', fontSize: '0.95rem', height: '30px', padding: '0.4rem 0.75rem', minWidth: '64px', lineHeight: 1.2, boxSizing: 'border-box', textTransform: 'none',}}>
+                                Log Out
+                            </Button>
+                    
+                            <Button onClick={handleAccountDeletion} sx={{ color: themeMode === 'light' ? 'black':'white', backgroundColor: 'rgba(78, 196, 4, 1)', fontSize: '0.95rem', height: '30px', padding: '0.4rem 0.75rem', minWidth: '64px', lineHeight: 1.2, boxSizing: 'border-box', textTransform: 'none',}}>
+                                Delete
+                            </Button>
+                        </Stack>
+                    
+                        <img src={themeMode === 'light' ? toggle_light : toggle_dark} alt="toggle theme" onClick={toggleTheme} style={{ width: '36px', marginLeft: '20px', cursor: 'pointer', filter: themeMode === 'light' ? 'none' : 'brightness(0) invert(1)', }}/>
+                    </Box>
+                </Toolbar>
+            </AppBar>
+
+        </ThemeProvider>
     </>
     );
 };
