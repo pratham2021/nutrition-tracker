1a996b519ac05c9c590090909789b610cd68cfe3
Pratham Hebbar <prathamhebbar2021@gmail.com>
2025-07-28 21:06:20 -0500

    push changes

--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,14 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Dashboard from '../src/dashboard/Dashboard.jsx'
+import Dashboard from './nutrition-tracker/Dashboard.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Router>
       <Routes>
         <Route path="/" element={<App/>}/>
-        <Route path="/dashboard" element={<Dashboard/>}/>
+        <Route path="/nutrition-tracker/" element={<Dashboard/>}/>
       </Routes>
     </Router>
   </StrictMode>,
