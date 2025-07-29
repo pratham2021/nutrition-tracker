838763b1cb28c5d6f70b2bee3a1318af1753fc92
Pratham Hebbar <prathamhebbar2021@gmail.com>
2025-07-28 22:55:22 -0500

    update

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/nutrition-tracker',
+  base: '/nutrition-tracker/',
   build: {
     chunkSizeWarningLimit: 1000
   }
